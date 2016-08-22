b609fd2df0f3075818a6b18fcd6efe7d7a961a92
Bryce Rogers <bryce.rogers321@gmail.com>
2016-08-22 21:49:11 +1000

    POC on shield activation

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public GameObject controllerRight;
+	public GameObject controllerLeft;
+
+	// Spell vars (maybe move)
+	private float sqrShieldDistance = 3.0f;
+
+	// Spell scripts
+	private Shield shield;
+
+	void Start () {
+		shield = GetComponent<Shield>();
+	}
+	
+	void Update () {
+
+		// Spell checks
+		float sqrControllerDistance = (controllerLeft.transform.position - controllerRight.transform.position).sqrMagnitude;
+		Debug.Log (sqrControllerDistance);
+		if(sqrControllerDistance > sqrShieldDistance){
+			shield.activate ();	
+		} else {
+			shield.deactivate ();
+		}
+	
+	}
+}
---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+
+	public GameObject shield;
+
+	private bool active;
+
+	void Start () {
+		shield.SetActive (false);
+	}
+	
+	void Update () {
+	}
+
+	public void activate() {
+		if (!active) {
+			active = true;
+			shield.SetActive (true);
+		}
+	}
+
+	public void deactivate() {
+		if(active) {
+			active = false;
+			shield.SetActive (false);
+		}
+	}
+}
