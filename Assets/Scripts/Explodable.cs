81e8e6586d84c22a0bbb8a4cea29f62a479b3ba1
Bryce Rogers <bryce.rogers321@gmail.com>
2016-10-03 22:14:28 +1100

    Shield Explode functional

---
+++ Assets/Scripts/Explodable.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explodable : MonoBehaviour {
+
+	private Rigidbody rigidBody;
+	private float force = 4000f;
+
+	void Start () {
+		rigidBody = GetComponent <Rigidbody> ();
+	}
+	
+	void OnTriggerEnter(Collider collider) {
+		rigidBody.AddExplosionForce (force, collider.transform.position, 10f);
+	}
+}
---
+++ Assets/Scripts/ShieldExplode.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldExplode : MonoBehaviour {
+
+	private bool exploding = false;
+	private float timeLeft;
+
+	public GameObject shieldExplodeObject;
+
+	void Start() {
+		shieldExplodeObject.SetActive (false);
+	}
+
+	void Update () {
+		if (exploding) {
+			timeLeft -= Time.deltaTime;
+			if (timeLeft < 0f) {
+				exploding = false;
+				shieldExplodeObject.SetActive (false);
+			}
+		}
+	}
+
+	public void explode() {
+		if (!exploding) {
+			exploding = true;
+			shieldExplodeObject.SetActive (true);
+			timeLeft = 0.5f;
+		}
+	}
+}
